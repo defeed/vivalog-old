add3332680f9934eb0dbcc3f5d35c85709f7e0c6
Artem Pakk <apakk@me.com>
2015-04-05 22:37:29 +0300

    Move projects_controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,45 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :volume, :price)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+end
