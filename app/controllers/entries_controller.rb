ed9d0deaa5d9d7cea321c2ec4d00e916cb188d6e
Artem Pakk <apakk@me.com>
2015-04-06 20:30:29 +0300

    Create entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,32 +1,16 @@
 class EntriesController < ApplicationController
-  before_action :find_entry, only: [:show, :edit, :update, :destroy]
-  before_action :find_project, only: [:index, :new, :create]
-
-  def index
-    @entries = @project.entries
-  end
+  before_action :find_entry, only: [:destroy]
 
   def new
-    @entry = @project.entries.new
+    @entry = Entry.new
   end
 
   def create
-    @entry = @project.entries.new(entry_params)
+    @entry = Entry.new(entry_params)
     if @entry.save
-      redirect_to project_entries_path(@entry.project)
+      redirect_to project_path(@entry.project)
     else
       render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @entry.update(entry_params)
-      redirect_to project_entries_path(@entry.project)
-    else
-      render :edit
     end
   end
 
@@ -40,12 +24,8 @@ class EntriesController < ApplicationController
 
   def entry_params
     params.require(:entry).permit(
-      :work_type, :workers, :coefficient, :worked_on
+      :project_id, :work_type, :workers, :coefficient, :worked_on
     )
-  end
-
-  def find_project
-    @project = Project.find(params[:id])
   end
 
   def find_entry
