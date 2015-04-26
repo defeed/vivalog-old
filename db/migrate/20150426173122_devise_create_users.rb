309523b5164c2f6a771e4c55058e15d0e79a0821
Artem Pakk <apakk@me.com>
2015-04-26 20:49:53 +0300

    Add name to users

--- db/migrate/20150426173122_devise_create_users.rb
+++ db/migrate/20150426173122_devise_create_users.rb
@@ -3,6 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       t.string :email, null: false, default: ''
       t.string :username, null: false, default: ''
+      t.string :name, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
       t.string :reset_password_token
       t.datetime :reset_password_sent_at
