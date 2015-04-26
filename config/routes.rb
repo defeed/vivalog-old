6a70eb209a6badfca519efadea73a55b22e87a33
Artem Pakk <apakk@me.com>
2015-04-26 22:19:49 +0300

    Change devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_up: 'signup',
+               sign_in: 'signin',
+               sign_out: 'signout'
+             }
   resources :projects
   resources :entries, only: [:new, :create, :destroy]
 
