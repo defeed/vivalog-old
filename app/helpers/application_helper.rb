32040fd8f246d49e45ddb9981d79715aeedef1e5
Artem Pakk <apakk@me.com>
2015-04-30 18:15:09 +0300

    Extract roles and work types builders to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,22 @@ module ApplicationHelper
   def bootstrap_class_for(flash_type)
     BOOTSTRAP_FLASH_MSGS.fetch(flash_type.to_sym, flash_type.to_s)
   end
+
+  def roles_for_select
+    hash = {}
+    User::ROLES.each do |role|
+      hash[I18n.t("roles.#{role}")] = role
+    end
+
+    hash
+  end
+
+  def work_types_for_select
+    hash = {}
+    Entry::WORK_TYPES.each do |type|
+      hash[I18n.t("work_types.#{type}")] = type
+    end
+
+    hash
+  end
 end
