9c99961f532ab7c1a3f1baec649738c9c63c583c
Artem Pakk <apakk@me.com>
2015-04-30 18:14:20 +0300

    Set work types as a constant

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,11 +3,5 @@ class Entry < ActiveRecord::Base
 
   validates_presence_of :project_id, :worked_on, :work_type, :coefficient
 
-  def self.work_types
-    {
-      'Receiving' => 'receive',
-      'Polishing' => 'polish',
-      'Other' => 'other'
-    }
-  end
+  WORK_TYPES = %i( receive polish other )
 end
