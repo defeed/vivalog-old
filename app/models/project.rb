26ccc0b2abe293e58816f3adca4f5cc20f49703e
Artem Pakk <apakk@me.com>
2015-04-30 22:05:56 +0300

    WIP for project finalization

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,4 +12,16 @@ class Project < ActiveRecord::Base
 
     result.order(date: :desc)
   end
+
+  def avg_workers
+    entries.map(&:workers).inject(:+).to_f / entries.count
+  end
+
+  def finalize
+    return [false, :no_entries] if entries.none?
+    return [false, :workers_not_eql] unless entries.count == avg_workers
+    return [false, :date_in_future] if date > Date.today
+    # TODO: implement stuff
+    true
+  end
 end
