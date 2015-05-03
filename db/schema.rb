275377d2d17b46b9c1e5b8f638ad390b0ba9df2f
Artem Pakk <apakk@me.com>
2015-05-03 16:14:36 +0300

    Add code to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501172443) do
+ActiveRecord::Schema.define(version: 20150503100918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150501172443) do
     t.datetime "updated_at",    null: false
     t.datetime "finalized_at"
     t.integer  "finalized_by"
+    t.string   "code"
   end
 
   create_table "users", force: :cascade do |t|
